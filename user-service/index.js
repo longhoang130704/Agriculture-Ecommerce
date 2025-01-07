ccb479a596a01a9ca462f1fbe1e6f4653f03a347
Trần Nguyễn Anh Khoa <trannguyenanhkhoa0104@gmail.com>
2025-01-07 22:41:31 +0700

    add frontend branch with vite-react tailwindcss shadcn/ui

--- user-service/index.js
+++
@@ -1,8 +0,0 @@
-import initServer from "./src/init/init.js";
-
-// export default function main() {
-//     initServer()
-// }
-
-initServer()
-
--- user-service/src/routers/v1/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import AuthService from '../../api/auth.api.js';
-
-const authRouter = express.Router();
-
-authRouter.post('/login', AuthService.login);
-
-export default authRouter;
