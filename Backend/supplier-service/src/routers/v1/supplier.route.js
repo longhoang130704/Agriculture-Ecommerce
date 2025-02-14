e4441c023cb4e38ea26b558e1eb37b1a3fa800c0
longhoang130704 <long.hoang130704@hcmut.edu.vn>
2025-02-14 17:43:53 +0700

    fix: delete authentication, authorization in user-service and supplier-service

--- Backend/supplier-service/src/routers/v1/supplier.route.js
+++ Backend/supplier-service/src/routers/v1/supplier.route.js
@@ -2,17 +2,24 @@ import express from 'express';
 import SupplierService from '../../api/supplier.api.js';
 
 // Auth Middleware
-import authentication from '../../middlewares/authentication.middleware.js';
 // import authorization from '../../middlewares/authorization.middleware.js';
 
 const supplierRouter = express.Router();
 
-supplierRouter.get('/restore/:supplierId', authentication, SupplierService.restore);
-supplierRouter.get('/destroy/:supplierId', authentication, SupplierService.destroy);
-supplierRouter.get('/:supplierId', authentication, SupplierService.getSupplierById);
-supplierRouter.put('/:supplierId', authentication, SupplierService.updateSupplierById);
-supplierRouter.delete('/:supplierId', authentication, SupplierService.deleteSupplierById);
+supplierRouter.get('/restore/:supplierId', SupplierService.restore);
+supplierRouter.get('/destroy/:supplierId', SupplierService.destroy);
+supplierRouter.get('/:supplierId', SupplierService.getSupplierById);
+supplierRouter.put('/:supplierId', SupplierService.updateSupplierById);
+supplierRouter.delete('/:supplierId', SupplierService.deleteSupplierById);
 supplierRouter.post('/', SupplierService.createSupplier);
-supplierRouter.get('/', authentication, SupplierService.getAllSuppliers);
+supplierRouter.get('/', SupplierService.getAllSuppliers);
+
+// supplierRouter.get('/restore/:supplierId', authentication, SupplierService.restore);
+// supplierRouter.get('/destroy/:supplierId', authentication, SupplierService.destroy);
+// supplierRouter.get('/:supplierId', authentication, SupplierService.getSupplierById);
+// supplierRouter.put('/:supplierId', authentication, SupplierService.updateSupplierById);
+// supplierRouter.delete('/:supplierId', authentication, SupplierService.deleteSupplierById);
+// supplierRouter.post('/', SupplierService.createSupplier);
+// supplierRouter.get('/', authentication, SupplierService.getAllSuppliers);
 
 export default supplierRouter;
